f10541cba537d1f117bc7fa6abc851304b306198
Wang <tianhao.wnag@vibrant-america.com>
2023-04-20 08:02:05 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use anyhow::{anyhow, Result};
 use clap::Parser;
 use colored::Colorize;
 use mime::Mime;
+use reqwest::header::{HeaderMap, AUTHORIZATION};
 use reqwest::{header, Client, Response, Url};
 use std::{collections::HashMap, str::FromStr};
 use syntect::{
     easy::HighlightLines,
     highlighting::{Style, ThemeSet},
     parsing::SyntaxSet,
-    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+    util::as_24_bit_terminal_escaped,
 };
 
 /// A native httpie implementation with Rust
@@ -24,19 +25,35 @@ enum SubCommand {
     Get(Get),
     Post(Post),
 }
+// get 子命令
+
+/// feed get with an url and we will retrieve the response for you
 #[derive(Parser, Debug)]
 struct Get {
+    /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    header: Vec<KvPair>,
+    query: Vec<KvPair>,
 }
+
+// post 子命令。需要输入一个 url，和若干个可选的 key=value，用于提供 json body
+
+/// feed post with an url and optional key=value pairs. We will post the data
+/// as JSON, and retrieve the response for you
 #[derive(Parser, Debug)]
 struct Post {
+    /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// HTTP 请求的 body
     #[clap(parse(try_from_str=parse_kv_pair))]
     body: Vec<KvPair>,
+    header: Vec<KvPair>,
 }
 
-/// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
+/// 命令行中的 key=value or key:value 可以通过 parse_kv_pair 解析成 KvPair 结构
 #[derive(Debug, PartialEq)]
 struct KvPair {
     k: String,
@@ -49,7 +66,10 @@ impl FromStr for KvPair {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // 使用 = 进行 split，这会得到一个迭代器
-        let mut split = s.split('=');
+        let mut split = s.split("=");
+        if s.contains(":=") {
+            split= s.split(":=");
+        }
         let err = || anyhow!(format!("Failed to parse {}", s));
         Ok(Self {
             // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
@@ -73,8 +93,16 @@ fn parse_url(s: &str) -> Result<String> {
 
 /// 处理 get 子命令
 async fn get(client: Client, args: &Get) -> Result<()> {
-    let resp = client.get(&args.url).send().await?;
-    Ok(print_resp(resp).await?)
+    let mut headers = HeaderMap::new();
+    for header in args.header.iter() {
+        //hard code header
+        if header.k.to_lowercase() == "authorization" {
+            headers.insert(AUTHORIZATION, header.v.parse()?);
+        }
+        // headers.insert(header, header.v.parse()?);
+    }
+    let resp = client.get(&args.url).query(query) .headers(headers).send().await?;
+    print_resp(resp).await
 }
 
 /// 处理 post 子命令
@@ -84,7 +112,7 @@ async fn post(client: Client, args: &Post) -> Result<()> {
         body.insert(&pair.k, &pair.v);
     }
     let resp = client.post(&args.url).json(&body).send().await?;
-    Ok(print_resp(resp).await?)
+    print_resp(resp).await
 }
 
 /// 将服务器返回的 content-type 解析成 Mime 类型
@@ -123,11 +151,16 @@ fn print_headers(resp: &Response) {
 fn print_body(m: Option<Mime>, body: &str) {
     match m {
         // 对于 "application/json" 我们 pretty print
-        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
-        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+        Some(v) if v.subtype() == mime::JSON => {
+            // jsonxf::Formatter::pretty_printer()
+            print_syntect(&jsonxf::pretty_print(body).unwrap(), "json");
+        }
+        Some(v) if v.subtype() == mime::HTML => {
+            print_syntect(body, "html")
+        }
 
         // 其它 mime type，我们就直接输出
-        _ => println!("{}", body),
+        _ => println!("{:?}, \r\n {}", m, body),
     }
 }
 
@@ -137,10 +170,10 @@ fn print_syntect(s: &str, ext: &str) {
     let ts = ThemeSet::load_defaults();
     let syntax = ps.find_syntax_by_extension(ext).unwrap();
     let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-    for line in LinesWithEndings::from(s) {
+    for line in s.lines() {
         let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
         let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
-        print!("{}", escaped);
+        println!("{}", escaped);
     }
 }
 
@@ -155,10 +188,42 @@ async fn main() -> Result<()> {
     let client = reqwest::Client::builder()
         .default_headers(headers)
         .build()?;
-    let result = match opts.subcmd {
+    match opts.subcmd {
         SubCommand::Get(ref args) => get(client, args).await?,
         SubCommand::Post(ref args) => post(client, args).await?,
     };
 
-    Ok(result)
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
 }
